b7c7dc71a7115f5856c4671345cf8b4324fae530
mingi <mintim01@naver.com>
2024-11-16 10:40:06 +0900

    feat: is_scraped 필드 추가

--- project_detail/views.py
+++ project_detail/views.py
@@ -25,19 +25,19 @@ class ProjectDetailViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, m
         return Response({'message': '프로젝트가 성공적으로 삭제되었습니다.'})
     
 
-    @action(methods=['GET'], detail=True, url_path="like")
-    def like(self, request, pk=None):
-        project = self.get_queryset().filter(id=pk).first()
-        if request.user.account in project.like_accounts.all():
-            project.like_accounts.remove(request.user.account)  # 이미 좋아요 누른 경우, 취소
-            project.like_count -= 1
-            project.save()
-        else:
-            project.like_accounts.add(request.user.account)  # 좋아요 추가
-            project.like_count += 1
-            project.save()
-        serializer = ProjectSerializer(project, context={'request': request})
-        return Response(serializer.data)
+    # @action(methods=['GET'], detail=True, url_path="like")
+    # def like(self, request, pk=None):
+    #     project = self.get_queryset().filter(id=pk).first()
+    #     if request.user.account in project.like_accounts.all():
+    #         project.like_accounts.remove(request.user.account)  # 이미 좋아요 누른 경우, 취소
+    #         project.like_count -= 1
+    #         project.save()
+    #     else:
+    #         project.like_accounts.add(request.user.account)  # 좋아요 추가
+    #         project.like_count += 1
+    #         project.save()
+    #     serializer = ProjectSerializer(project, context={'request': request})
+    #     return Response(serializer.data)
     
     @action(methods=['GET'], detail=True, url_path="scrap")
     def scrap(self, request, pk=None):
